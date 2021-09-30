aee6c8e5bb186b4663146325d611ea9e3df044d4
soso01 <6125024@gmail.com>
2021-10-01 00:11:58 +0900

    utterances 키 변경

--- site.config.js
+++ site.config.js
@@ -32,7 +32,7 @@ module.exports = {
   imageCDNHost: null,
 
   // Utteranc.es comments via GitHub issue comments (optional)
-  utterancesGitHubRepo: 'https://github.com/soso01/notion-blog/issues/1',
+  utterancesGitHubRepo: 'soso01/notion-blog',
 
   // whether or not to enable support for LQIP preview images (optional)
   // NOTE: this requires you to set up Google Firebase and add the environment
