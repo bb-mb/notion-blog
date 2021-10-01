2df51800e9e329cbf4b7af81ec498af8e292dfb4
soso01 <6125024@gmail.com>
2021-10-01 10:16:03 +0900

    url path 정규식 수정

--- lib/get-canonical-page-id.ts
+++ lib/get-canonical-page-id.ts
@@ -60,7 +60,6 @@ export const getCanonicalPageIdImpl = (
 export const normalizeTitle = (title: string | null): string => {
   return (title || '')
     .replace(/ /g, '-')
-    .replace(/[^a-zA-Z0-9가-힣-]/g, '')
     .replace(/--/g, '-')
     .replace(/-$/, '')
     .replace(/^-/, '')
