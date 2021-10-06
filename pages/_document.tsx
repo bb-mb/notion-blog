a242dd75c1fe0217fab6184936e7f99d9e05a8d2
soso01 <6125024@gmail.com>
2021-10-06 20:42:39 +0900

    favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,19 +11,19 @@ export default class MyDocument extends Document {
             <link
               rel='apple-touch-icon'
               sizes='180x180'
-              href='/apple-touch-icon.png'
+              href='/apple-touch-icon.png?v=3'
             />
             <link
               rel='icon'
               type='image/png'
               sizes='32x32'
-              href='/favicon-32x32.png'
+              href='/favicon-32x32.png?v=3'
             />
             <link
               rel='icon'
               type='image/png'
               sizes='16x16'
-              href='/favicon-16x16.png'
+              href='/favicon-16x16.png?v=3'
             />
             <link rel='manifest' href='/site.webmanifest' />
             <link
@@ -32,7 +32,7 @@ export default class MyDocument extends Document {
               color='#5bbad5'
             />
             <meta name='msapplication-TileColor' content='#da532c' />
-            <meta name='theme-color' content='#ffffff' />
+            {/* <meta name='theme-color' content='#ffffff' /> */}
 
             <script
               async
